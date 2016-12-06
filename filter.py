be2a3d9423add4157dbf39df65973d7ed4a46952
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2016-12-05 22:10:03 -0500

    remove years 2005, 2006, 2007 for low review counts

--- filter.py
+++ filter.py
@@ -58,17 +58,18 @@ for review_num, review in enumerate(reviews):
     review = json.loads(review)
     bid = review['business_id'].encode('utf-8')
     if bid in common_restaurants:
-        reviews_count += 1
         review_year = review['date'].split('-')[0]
-        review_dir = DIR + review_year + '/'
-        review_filename = 'review'+str(review_num)
-        review_to_bid[review_filename] = str(bid)
-        if not os.path.exists(review_dir):
-            os.makedirs(review_dir)
-        review_path = review_dir + review_filename + '.txt'
+        if int(review_year) >= 2008:
+            reviews_count += 1
+            review_dir = DIR + review_year + '/'
+            review_filename = 'review'+str(review_num)
+            review_to_bid[review_filename] = str(bid)
+            if not os.path.exists(review_dir):
+                os.makedirs(review_dir)
+            review_path = review_dir + review_filename + '.txt'
 
-        with open(review_path, 'w') as review_file:
-            review_file.write(review['text'].encode('utf-8'))
+            with open(review_path, 'w') as review_file:
+                review_file.write(review['text'].encode('utf-8'))
 
 with open(FILE_REVIEW_BUS, 'w') as rb_file:
     pickle.dump(review_to_bid, rb_file)
